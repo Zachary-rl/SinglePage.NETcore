b8bcf97ecc055283c3c992cfe7c60f39a80679ca
Zachary Lynch <58271558+Zachary-rl@users.noreply.github.com>
2020-07-11 12:33:39 -0700

    Create Migrations.Config.cs

---
+++ Migrations.Config.cs
@@ -0,0 +1,38 @@
+namespace WebApplication1.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using WebApplication1.Models;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WebApplication1.Models.WebApplication1Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(WebApplication1.Models.WebApplication1Context context)
+        {
+            context.ShoppingLists.AddOrUpdate(
+                
+
+                new ShoppingList
+                {
+                    Name = "Groceries",
+                    Items =
+                    {
+                        new Item {Name="Milk" },
+                        new Item {Name="Steak" },
+                        new Item {Name="Eggs" },
+                    }
+                },
+                new ShoppingList
+                {
+                    Name = "Hardware"
+                }
+                );
+        }
+    }
+}
